807aa1e96a9d6453c90992c455af4a2933068ade
Anna <anafbtest5@gmail.com>
2019-07-30 13:52:04 -0400

    tests

--- src/test/java/BaseUI.java
+++ src/test/java/BaseUI.java
@@ -1,9 +1,16 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.asserts.SoftAssert;
+
+import java.lang.reflect.Method;
 
 public class BaseUI {
     WebDriver driver;
@@ -14,14 +21,41 @@ public class BaseUI {
     SalaryPage salaryPage;
     SignUpPage signUpPage;
     UploadResumePage uploadResumePage;
+    SoftAssert softAssert = new SoftAssert();
 
 
 
     @BeforeMethod
-    public void setUp() {
+    @Parameters("browser")
+    public void setup (@Optional("chrome") String browser, Method method) throws Exception {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+    //Check if parameter passed from TestNG is 'firefox'
+    if (browser.equalsIgnoreCase("firefox")) {
+        //Create firefox instance
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    // Check if parameter passed 'chrome'
+    else if (browser.equalsIgnoreCase("chrome")) {
+
+        //Set path to chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        //Create chrome instance
         driver = new ChromeDriver();
+        driver.get("chrome://settings/clearBrowserData");
+    }else if (browser.equalsIgnoreCase("IE")) {
+
+        System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
+        driver.manage().deleteAllCookies();
+
+    }else {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("chrome://settings/clearBrowserData");
+    }
+
         wait = new WebDriverWait(driver, 20);
         mainPage = new MainPage(driver, wait);
         loginPage = new LoginPage(driver, wait);
--- src/test/java/SalaryTests.java
+++ src/test/java/SalaryTests.java
@@ -12,15 +12,15 @@ public class SalaryTests extends BaseUI {
         driver.findElement(Locators.LINK_SALARY).click();
         currentUrlSalary = driver.getCurrentUrl();
         System.out.println(currentUrlSalary);
-        Assert.assertEquals(currentUrlSalary, Data.expectedUrlSalary);
-
+        //  Assert.assertEquals(currentUrlSalary, Data.expectedUrlSalary);
+        softAssert.assertEquals(currentUrlSalary, Data.expectedUrlSalary, "URL is wrong");
+        softAssert.assertAll();
 
     }
 
     public void validateAssertions(){
         Assert.assertEquals("Web", "WEb is");
         Assert.assertTrue(driver.findElement(By.xpath("//a")).isDisplayed(), "Element is not displayed");
-
         Assert.fail("Element is not displayed");
 
     }
